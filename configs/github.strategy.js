e3e782b9bf8a09322af776c0e5a12bb5e6cba6cc
Nelson Carvalho <nelsoncarvalhosf@gmail.com>
2019-11-13 19:08:27 -0300

    Include login GitHub

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -4,32 +4,32 @@ const GitHubStrategy = require("passport-github").Strategy;
 passport.use(
   new GitHubStrategy(
     {
-      clientID: "GITHUB_CLIENT_ID",
-      clientSecret: "GITHUB_CLIENT_SECRET",
+      clientID: "Iv1.7a0bedbca3957e3f",
+      clientSecret: "3e5d316cadbd3a261b8ca957c163954abbfe29ff",
       callbackURL: "http://localhost:3000/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       const onError = () => {
-        console.log('Ocorreu um erro!')
-      }
+        console.log("Ocorreu um erro!");
+      };
 
-      return done(onError, profile);
+      return done(undefined, profile);
     }
   )
 );
 
 passport.serializeUser(function(user, done) {
   const onError = () => {
-    console.log('Ocorreu um erro!')
-  }
+    console.log("Ocorreu um erro!");
+  };
 
-  done(onError, user);
+  done(undefined, user);
 });
 
 passport.deserializeUser(function(user, done) {
   const onError = () => {
-    console.log('Ocorreu um erro!')
-  }
+    console.log("Ocorreu um erro!");
+  };
 
-  done(onError, user);
+  done(undefined, user);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    return next();
+  }
+  // denied. redirect to login
+  res.redirect("/");
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/auth/github", passport.authenticate("github"));
+
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/admin");
+  }
+);
+
+router.get("/admin", ensureAuthenticated, function(req, res) {
+  res.render("admin", { user: req.session.passport.user });
 });
 
 module.exports = router;
